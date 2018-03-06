7a934b1fce9fe7ba99914ee73f95acaf4029cd75
Alias44 <25497382+Alias44@users.noreply.github.com>
2018-03-06 05:02:49 -0700

    Inital Upload
    Inital Upload of Snowy Trees

---
+++ Nandonalt_SnowyTrees-b18/Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/PlantGraphicPatch.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using RimWorld;
+using Verse;
+
+
+namespace Nandonalt_SnowyTrees
+{
+	[StaticConstructorOnStartup]
+	public class HarmonyPatches
+	{
+		static readonly Type patchType = typeof(HarmonyPatches);
+
+		static HarmonyPatches ()
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("Nandonalt_SnowyTrees.main");
+			harmony.Patch(AccessTools.Method(typeof(Plant), "get_Graphic"), null, new HarmonyMethod(patchType, nameof(SnowyGraphic)));
+
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[HarmonyPostfix]
+		public static void SnowyGraphic (Plant __instance, ref Graphic __result)
+		{
+			if (PlantGraphicSupportDefinitions.SnowyPlantsNormal.Contains(__instance.def.defName)
+				&& (!(__instance.def.plant.leaflessGraphic != null && __instance.LeaflessNow) || PlantGraphicSupportDefinitions.SnowyPlantsLeafless.Contains(__instance.def.defName))
+				&& !(__instance.LifeStage == PlantLifeStage.Sowing)
+				)
+			{
+				// Switch graphic to snowy variant:
+				if (__instance.Map.snowGrid.GetDepth(__instance.Position) >= 0.5f)
+				{
+					ThingDef parentDef = __instance.def;
+					string newPath = null;
+					Graphic snowyGraphic;
+
+					// If supporting leafless graphic:
+					if (__instance.LeaflessNow && PlantGraphicSupportDefinitions.SnowyPlantsLeafless.Contains(__instance.def.defName))
+					{
+						newPath = parentDef.graphicData.texPath.Replace("Things/Plant/", "Things/Plant_Snowy_Leafless/");
+					}
+					// else, if immature variant and supported:
+					else if (__result.path.ToLowerInvariant().Contains("immature")
+						&& PlantGraphicSupportDefinitions.SnowyPlantsImmature.Contains(__instance.def.defName)
+						)
+					{
+						newPath = parentDef.graphicData.texPath.Replace("Things/Plant/", "Things/Plant_Snowy_Immature/");
+					}
+					// Otherwise we show the snowy default / mature variant.
+					else
+					{
+						newPath = parentDef.graphicData.texPath.Replace("Things/Plant/", "Things/Plant_Snowy/");
+					}
+
+					snowyGraphic = GraphicDatabase.Get<Graphic_Random>(newPath, __result.Shader, __result.drawSize, __result.Color, __result.ColorTwo, __result.data);
+
+					__result = snowyGraphic;
+				}
+			}
+		}
+
+		#region A16: HugsLib detour:
+		/*using HugsLib.Source.Detour;
+
+		[StaticConstructorOnStartup]
+		public class PlantTree : Plant
+		{
+			private static Graphic GraphicSowing = GraphicDatabase.Get<Graphic_Single>("Things/Plant/Plant_Sowing", ShaderDatabase.Cutout, Vector2.one, Color.white);
+
+			[DetourProperty(typeof(Plant), "Graphic", DetourProperty.Getter)]
+			public override Graphic Graphic
+			{
+				get
+				{
+					if (this.LifeStage == PlantLifeStage.Sowing)
+					{
+						return GraphicSowing;
+					}
+					if (this.def.plant.leaflessGraphic != null && this.LeaflessNow)
+					{
+						return this.def.plant.leaflessGraphic;
+					}
+					if (TreeInjector.alteredPlants.Contains(this.def.defName))
+						{
+						if (this.Map.snowGrid.GetDepth(this.Position) >= 0.5f)
+						{
+							ThingDef parentDef = this.def;
+							Graphic Snowy = GraphicDatabase.Get(parentDef.graphicData.graphicClass, parentDef.graphicData.texPath + "_Snowy", parentDef.graphic.Shader, parentDef.graphicData.drawSize, parentDef.graphicData.color, parentDef.graphicData.colorTwo);
+							return Snowy;
+						}
+					}
+					return this.def.graphicData.GraphicColoredFor(this);
+				}
+			}
+		}*/
+		#endregion
+	}
+}
+
---
+++ Nandonalt_SnowyTrees-b18/Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/PlantGraphicSupportDefinitions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace Nandonalt_SnowyTrees
+{
+    /// <summary>
+    /// Definitions for what textures are supported for which plants.
+    /// Basic texture is needed to even enter the logic for picking a new texture.
+    /// Textures have been sepparated due to issues with the game engine understanding all textures in the same folder
+    /// as alternatives instead of textures for different states. Even if following vanilla folder structure, leafless 
+    /// textures cause issues loading other textures for the same plant even without any logic or added definitions.
+    /// 
+    /// TODO:
+    /// Should probably look into defining these through xml files instead.
+    /// </summary>
+    public static class PlantGraphicSupportDefinitions
+    {
+        /// <summary>
+        /// Planet definition names which have basic graphics support.
+        /// Textures located in "Textures\Things\Plant_Snowy" folder.
+        /// </summary>
+        public static readonly List<string> SnowyPlantsNormal = new List<string>(new string[]
+        {
+            "PlantRaspberry",
+            "PlantBush",
+            "PlantTreeOak",
+            "PlantTreePoplar",
+            "PlantTreePine",
+            "PlantTreeBirch",
+            "PlantTreeTeak",
+            "PlantTreeCecropia"
+        });
+
+        /// <summary>
+        /// Planet definition names which have graphics for immature state.
+        /// Textures located in "Textures\Things\Plant_Snowy_Immature" folder.
+        /// /// Internal structure is the same as for basic textures.
+        /// </summary>
+        public static readonly List<string> SnowyPlantsImmature = new List<string>(new string[]
+        {
+            "PlantRaspberry",
+        });
+
+        /// <summary>
+        /// Plant definition names which have graphics for leafless state.
+        /// Textures located in "Textures\Things\Plant_Snowy_Leafless" folder.
+        /// Internal structure is the same as for basic textures.
+        /// </summary>
+        public static readonly List<string> SnowyPlantsLeafless = new List<string>(new string[]
+        {
+            "PlantBush",
+        });
+    }
+}
