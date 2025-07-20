c8af16d9b45830f6c7fba463b7f2b5f180bcca1a
Alias <25497382+Alias44@users.noreply.github.com>
2025-07-19 19:34:57 -0700

    Whitespace
    Convert to document namespace scope

--- Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/PlantGraphicPatch.cs
+++ Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/PlantGraphicPatch.cs
@@ -5,62 +5,61 @@ using RimWorld;
 using Verse;
 
 
-namespace Nandonalt_SnowyTrees
+namespace Nandonalt_SnowyTrees;
+
+[StaticConstructorOnStartup]
+public class HarmonyPatches
 {
-	[StaticConstructorOnStartup]
-	public class HarmonyPatches
+	static readonly Type patchType = typeof(HarmonyPatches);
+
+	static HarmonyPatches ()
 	{
-		static readonly Type patchType = typeof(HarmonyPatches);
+		var harmony = new Harmony("Nandonalt_SnowyTrees.main");
+		harmony.Patch(AccessTools.Method(typeof(Plant), "get_Graphic"), null, new HarmonyMethod(patchType, nameof(SnowyGraphic)));
 
-		static HarmonyPatches ()
+		harmony.PatchAll(Assembly.GetExecutingAssembly());
+	}
+
+	[HarmonyPostfix]
+	public static void SnowyGraphic (Plant __instance, ref Graphic __result)
+	{
+		if (__instance.Spawned &&
+		    __instance.Map.snowGrid.GetDepth(__instance.Position) >= 0.5f &&
+		    __instance.LifeStage != PlantLifeStage.Sowing)
 		{
-			var harmony = new Harmony("Nandonalt_SnowyTrees.main");
-			harmony.Patch(AccessTools.Method(typeof(Plant), "get_Graphic"), null, new HarmonyMethod(patchType, nameof(SnowyGraphic)));
+			string newPath = null;
 
-			harmony.PatchAll(Assembly.GetExecutingAssembly());
-		}
+			SnowyPlantDef candidatePlant = DefDatabase<SnowyPlantDef>.GetNamed(__instance.def.defName, false);
 
-		[HarmonyPostfix]
-		public static void SnowyGraphic (Plant __instance, ref Graphic __result)
-		{
-			if (__instance.Spawned &&
-			    __instance.Map.snowGrid.GetDepth(__instance.Position) >= 0.5f &&
-			    __instance.LifeStage != PlantLifeStage.Sowing)
+			if (candidatePlant != null)
 			{
-				string newPath = null;
-
-				SnowyPlantDef candidatePlant = DefDatabase<SnowyPlantDef>.GetNamed(__instance.def.defName, false);
-
-				if (candidatePlant != null)
+				// If the tree is currently leafless and a leafless graphic exists
+				if (__instance.LeaflessNow && __instance.def.plant.leaflessGraphic != null)
 				{
-					// If the tree is currently leafless and a leafless graphic exists
-					if (__instance.LeaflessNow && __instance.def.plant.leaflessGraphic != null)
+					// (nested if prevents Jesus trees: leafless trees getting the wrong graphic because a leafless snowy texture doesn't exist):
+					if (candidatePlant.LeaflessSnowyPath != null)
 					{
-						// (nested if prevents Jesus trees: leafless trees getting the wrong graphic because a leafless snowy texture doesn't exist):
-						if (candidatePlant.LeaflessSnowyPath != null)
-						{
-							newPath = candidatePlant.LeaflessSnowyPath;
-						}
+						newPath = candidatePlant.LeaflessSnowyPath;
 					}
-					// else, if immature variant and supported:
-					else if (__result.path.ToLowerInvariant().Contains("immature")
-						&& (candidatePlant.ImmatureSnowyPath != null))
-					{
-						newPath = candidatePlant.ImmatureSnowyPath;
-					}
-					// Otherwise we show the snowy default / mature variant if it is supported.
-					else if (candidatePlant.RegularSnowyPath != null)
-					{
-						newPath = candidatePlant.RegularSnowyPath;
-					}
+				}
+				// else, if immature variant and supported:
+				else if (__result.path.ToLowerInvariant().Contains("immature")
+					&& (candidatePlant.ImmatureSnowyPath != null))
+				{
+					newPath = candidatePlant.ImmatureSnowyPath;
+				}
+				// Otherwise we show the snowy default / mature variant if it is supported.
+				else if (candidatePlant.RegularSnowyPath != null)
+				{
+					newPath = candidatePlant.RegularSnowyPath;
+				}
 
-					//If the texture has something to replace
-					if (newPath != null)
-					{
-						var snowyGraphic = GraphicDatabase.Get<Graphic_Random>(newPath, __result.Shader, __result.drawSize, __result.Color, __result.ColorTwo, __result.data);
+				//If the texture has something to replace
+				if (newPath != null)
+				{
+					var snowyGraphic = GraphicDatabase.Get<Graphic_Random>(newPath, __result.Shader, __result.drawSize, __result.Color, __result.ColorTwo, __result.data);
 
-						__result = snowyGraphic;
-					}
+					__result = snowyGraphic;
 				}
 			}
 		}
--- Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/SnowyPlantDef.cs
+++ Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/SnowyPlantDef.cs
@@ -1,29 +1,10 @@
 ﻿using Verse;
 
-namespace Nandonalt_SnowyTrees
+namespace Nandonalt_SnowyTrees;
+
+public class SnowyPlantDef : Def
 {
-	public class SnowyPlantDef : Def
-	{
-		private string leaflessSnowyPath = null;
-		private string immatureSnowyPath = null;
-		private string regularSnowyPath = null;
-
-		public string LeaflessSnowyPath
-		{
-			get { return leaflessSnowyPath; }
-			set { leaflessSnowyPath = value; }
-		}
-
-		public string ImmatureSnowyPath
-		{
-			get { return immatureSnowyPath; }
-			set { immatureSnowyPath = value; }
-		}
-
-		public string RegularSnowyPath
-		{
-			get { return regularSnowyPath; }
-			set { regularSnowyPath = value; }
-		}
-	}
+	public string leaflessSnowyPath = null;
+	public string immatureSnowyPath = null;
+	public string regularSnowyPath = null;
 }
