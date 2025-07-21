f3653de077aca6a502eae33b874cf67ac5737a4c
Alias <25497382+Alias44@users.noreply.github.com>
2025-07-20 19:49:14 -0700

    Restructured xml
    Changed data structure to make snowy plant definitions a less repetitive. Also cleans up code and allows future expansion to handle other game states (like polluted trees).

---
+++ Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/SnowyGraphic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml;
+
+namespace Nandonalt_SnowyTrees;
+
+public enum PlantGraphic : byte
+{
+	Regular = 0,
+	Immature,
+	Leafless,
+}
+
+public class SnowyGraphic
+{
+	public PlantGraphic plantGraphic;
+	public string path;
+
+	public SnowyGraphic() { }
+
+	public void GeneratePath(string basePath, string name)
+	{
+		string suffix = plantGraphic switch
+		{
+			PlantGraphic.Leafless => "_Leafless",
+			PlantGraphic.Immature => "_Immature",
+
+			_ => "",
+		};
+		path = basePath + name + suffix;
+	}
+
+	public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+	{
+		plantGraphic = (PlantGraphic) Enum.Parse(typeof(PlantGraphic), xmlRoot.Name);
+
+		if (xmlRoot.HasChildNodes)
+		{
+			path = xmlRoot.FirstChild.Value;
+		}
+	}
+}
--- Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/SnowyPlantDef.cs
+++ Source/Nandonalt_SnowyTrees/Nandonalt_SnowyTrees/SnowyPlantDef.cs
@@ -1,4 +1,6 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
 
 namespace Nandonalt_SnowyTrees;
 
@@ -7,4 +9,48 @@ public class SnowyPlantDef : Def
 	public string leaflessSnowyPath = null;
 	public string immatureSnowyPath = null;
 	public string regularSnowyPath = null;
+	public string baseGraphicPath;
+
+	public List<SnowyGraphic> graphics = [];
+
+	public Dictionary<PlantGraphic, Graphic> graphicDB = [];
+
+	public Graphic Graphic => graphicDB.GetValueOrDefault(PlantGraphic.Regular, null);
+	public Graphic ImmatureGraphic => graphicDB.GetValueOrDefault(PlantGraphic.Immature, null);
+	public Graphic LeaflessGraphic => graphicDB.GetValueOrDefault(PlantGraphic.Leafless, null);
+
+	public override void PostLoad()
+	{
+		var autoPath = graphics.Where(g => g.path.NullOrEmpty());
+
+		if (autoPath.Any() && (baseGraphicPath.NullOrEmpty() || this.label.NullOrEmpty()))
+		{
+			Log.Error($"Attempted Snowy Trees auto pathing for {this.defName} without defining base path or label");
+			return;
+		}
+
+		foreach (var data in autoPath)
+		{
+			data.GeneratePath(baseGraphicPath, this.label);
+		}
+	}
+
+	public override void ResolveReferences()
+	{
+		base.ResolveReferences();
+
+		ThingDef parentDef = DefDatabase<ThingDef>.GetNamed(this.defName);
+
+
+		foreach (var item in graphics)
+		{
+			if (!item.path.NullOrEmpty())
+			{
+				LongEventHandler.ExecuteWhenFinished(delegate
+				{
+					graphicDB[item.plantGraphic] = GraphicDatabase.Get(parentDef.graphicData.graphicClass, item.path, parentDef.graphic.Shader, parentDef.graphicData.drawSize, parentDef.graphicData.color, parentDef.graphicData.colorTwo);
+				});
+			}
+		}
+	}
 }
